96870638825c3d1ca08359a51af77003fb6be699
Spikree <avimahalingpure10@gmail.com>
2024-02-08 20:05:52 +0530

    sign in functionality added

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.29.209:3000';
+String uri = '';
 
 class GlobalVariables {
   // COLORS
--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -3,9 +3,13 @@ import 'dart:convert';
 import 'package:amazon_clone/constants/error_handeling.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   // Sign up the user
@@ -63,7 +67,16 @@ class AuthService {
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () {},
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            HomeScreen.routeName,
+            (route) => false,
+          );
+        },
       );
     } catch (e) {
       showSnackBar(context, e.toString());
